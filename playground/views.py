3c14e6286e09c7539d089f17b0cee0ea2c51ae4e
Milos Tasic <tasicm.dev@gmail.com>
2024-08-09 09:26:12 +0200

    Finish send_mail section

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
+from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
+    try:
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Mosh'}
+        )
+        message.send(['john@email.com'])
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
